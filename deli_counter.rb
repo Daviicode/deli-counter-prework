67a2482c7c4f425e2200754bbb7d12bd42d42003
David zhang <d.ydzdavid@yahoo.com>
2019-03-20 07:47:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line (katz_deli)
     if katz_deli.length == 0 
     puts "The line is currently empty."
     else
+      linez = []
      katz_deli.each.with_index(1) do |name, index|
       linez.push("#{index}. #{name}")
     end
